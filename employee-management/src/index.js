034cfa4807483080501be161de1c7cf8d3a062e5
Pratik Singh <singhpratik0001@gmail.com>
2024-03-13 23:06:28 +0530

    initial setup added

--- employee-management/src/index.js
+++ employee-management/src/index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+(async function() {
+    const data = await fetch("./src/data.json");
+    const res = await data.json();
+
+    let employee = res;
+    let selectedEmployeeId = employee[0].id;
+    let selectedEmployee = employee[0];
+    console.log(res)
+})();
